fecb7176eb4b5decfd4b42ad58aa774b21e4e3d7
a-bolz <andreasbolz@gmail.com>
2017-01-18 19:16:45 +0100

    create profiles controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   devise_for :users
+
+  resources :profiles, only: [:new, :edit, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
