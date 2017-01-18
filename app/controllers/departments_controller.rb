f492a6b773cf648f41cc88920c1aa5d801e572db
vincentstoop <vincentstoop@gmail.com>
2017-01-18 22:35:05 +0100

    Add New and Edit pages, Update index and show pages.

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -7,4 +7,37 @@ class DepartmentsController < ApplicationController
   def show
     @department = Department.find(params[:id])
   end
+
+  def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(department_params)
+
+    if @department.save
+      redirect_to @department, notice: "Department was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @department = Department.find(params[:id])
+  end
+
+  def update
+    @department = Department.find(params[:id])
+
+    if @department.update_attributes(department_params)
+      redirect_to @department
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def department_params
+    params.require(:department).permit(:name, :description, :image_url)
+  end
 end
