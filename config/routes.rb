6414b70b0cefbb1ee84edc09b993f162771afe08
vincentstoop <vincentstoop@gmail.com>
2017-01-18 20:53:08 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   devise_for :users
+  resources :profiles
+  resources :departments
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
