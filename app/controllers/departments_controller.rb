17334cc7980266abdfe0ba0277cb2d3122b24546
vincentstoop <vincentstoop@gmail.com>
2017-01-18 21:36:42 +0100

    Add Index and Show method to DepartmentsController. Add index.html.erb

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,10 @@
 class DepartmentsController < ApplicationController
+
+  def index
+    @departments = Department.all
+  end
+
+  def show
+    @department = Department.find(params[:id])
+  end
 end
