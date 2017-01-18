37914067c9b1a09a948a50cf6a19f2e3e32c221b
a-bolz <andreasbolz@gmail.com>
2017-01-18 17:37:46 +0100

    adding profile and adding profile associations and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :profile
+
+  def has_profile?
+    profile.present? && !profile.id.nil?
+  end
+
+  def full_name
+    profile.full_name
+  end
 end
---
+++ db/migrate/20170118163205_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :street_name
+      t.integer :house_number
+      t.string :house_number_addition
+      t.string :zip_code
+      t.string :city
+      t.string :phone_number
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
