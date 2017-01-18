f965567d69c6a512356648f282a9687ed5d0dd6f
KellyHuliselan <kellyhuliselan@gmail.com>
2017-01-18 21:27:27 +0100

    add index controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :profiles, only: [:show, :index]
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
