b8cb52ca236e1a2e76e8b15b2cd2a32d998e3937
Melvin Zehl <mhzehl@gmail.com>
2017-01-19 08:22:17 +0100

    Add new, create and update actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,33 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @page.save
+      redirect_to @page
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update_attributes(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end    
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :reference_number, :image_url, :video_url, :availability, :price, :department)
+  end
 end
