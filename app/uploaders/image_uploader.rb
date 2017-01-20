8e8bf4e57b0cfcb66df8c80201c9fdb1a9defb5e
Melvin Zehl <mhzehl@gmail.com>
2017-01-20 16:48:21 +0100

    Add styling

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :thumbnail do
     eager
+    resize_to_fit(150, 150)
+    cloudinary_transformation :quality => 80
+  end
+
+  version :medium do
+    eager
     resize_to_fit(300, 300)
     cloudinary_transformation :quality => 80
   end
