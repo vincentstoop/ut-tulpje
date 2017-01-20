1a16e181a4eb92775b50d55f06507979dfe8bb9f
KellyHuliselan <kellyhuliselan@gmail.com>
2017-01-20 15:56:45 +0100

    added validations and tests to product model and spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
   belongs_to :department
   has_many :photos
 
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 100}
+
   def has_photo?
     photo.present?
   end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a name" do
+      product = Product.new(name: "")
+      product.valid?
+      expect(product.errors).to have_key(:name)
+    end
+  end
+
+  it "is invalid with a listing name longer than 100 characters" do
+      product = Product.new(description: "")
+      product.valid?
+      expect(product.errors).to have_key(:description)
+    end
 end
