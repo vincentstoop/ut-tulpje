16f840d4e9c72a5cca8040d8dfd8db6816006c33
vincentstoop <vincentstoop@gmail.com>
2017-01-25 11:21:08 +0100

    Add ShoppingCart (.. items in cart) to all pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-#   helper_method :current_user, :logged_in?
-#
-# def current_user
-#   @current_user ||= User.find_by_id(session[:user])
-# end
-#
-#  def logged_in?
-#    current_user != nil
-#  end
-# The above helper methods will be relevant later in the context
-# of sessions and when we want only the current user to be
-# able to edit his own profile
+
+  before_action :init_shopping_cart
+
+  def init_shopping_cart
+    @shopping_cart = session[:shoppingcart] || {}
+    @shopping_cart_items = {}
+    @total_price = 0
+    # @shopping_cart = {"1" => 5, "3" => 2}
+    @number_of_cart_items = 0
+    @shopping_cart.each_value do |quantity|
+      @number_of_cart_items += quantity
+    end
+    shopping_cart_keys = @shopping_cart.keys
+    shopping_cart_keys.each do |product|
+      @shopping_cart_items[product] = Product.find product.to_i
+      @total_price += @shopping_cart_items[product].price * @shopping_cart[product]
+    end
+  end
 end
